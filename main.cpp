71d5891c870d794e55dd3397e1729359e598f9fb
Vishnenko Maxim <vishnenko.maxim@gmail.com>
2023-04-04 08:55:27 +0500

    add comments

--- main.cpp
+++ main.cpp
@@ -59,11 +59,16 @@ void Print_IP(const Container<Type, Allocator> &value){
     cout << endl;
 }
 
+/// Template function to print IP given as a tuple
+/// \tparam T tuple's elements type
+/// \tparam Container must be tuple
+/// \tparam T_elem type of first element
+/// \param value tuple
 template <class ... T, class Container = tuple<T...>, class T_elem = typename tuple_element<0, tuple<T...>>::type,
         class = enable_if_t<is_same_v<typename tuple_element<1, tuple<T...>>::type, T_elem>
                             && is_same_v<typename tuple_element<2, tuple<T...>>::type, T_elem>
                             && is_same_v<typename tuple_element<3, tuple<T...>>::type, T_elem>>>
-void Print_IP(const tuple<T...> value){
+void Print_IP(const tuple<T...> &value){
     cout << get<0>(value) << ".";
     cout << get<1>(value) << ".";
     cout << get<2>(value) << ".";
